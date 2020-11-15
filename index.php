621026acc972b6ae599a29d9da5615298db16dc4
diraneh <diraneh>
2020-11-15 20:01:59 +0100

    portefolio

---
+++ index.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="Fr">
+    <head>
+    	<title>Elmi</title>
+
+    	<!-- meta -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- stylesheet -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/owl.carousel.css">
+		<link rel="stylesheet" href="css/owl.theme.min.css">
+        <link rel="stylesheet" href="css/animate.css">
+		<link rel="stylesheet" href="css/main.css">
+
+		<!-- google font -->
+        <link href='http://fonts.googleapis.com/css?family=Oswald:300,400' rel='stylesheet'>
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,300' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Oxygen:400,300' rel='stylesheet' type='text/css'>
+        <link href="http://fonts.googleapis.com/css?family=Rouge+Script" rel="stylesheet" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Milonga' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <div class="content-block" id="header">
+            <div id="overlay-1">
+                <header id="site-header" class="clearfix">
+                    <div class="pull-left">
+                        <h1><a href="#">Elmi Diraneh 0751413840</a></h1>
+                    </div>
+                    <div class="pull-right">
+                        <nav class="navbar site-nav" role="navigation">
+                            <!-- Brand and toggle get grouped for better mobile display -->
+                           
+
+                            <!-- Collect the nav links, forms, and other content for toggling -->
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                <ul class="nav navbar-nav">
+                                    <li class="active"><a href="#header"><i class="fa fa-home"></i> <span>Accueil</span></a></li>
+                                    <li><a href="#portfolio"><i class="fa fa-bookmark"></i> Portfolio</a></li>
+                                    <li><a href="#services"><i class="fa fa-bullhorn"></i>À propos de moi</a></li>
+                                    <li><a href="#testimonials"><i class="fa fa-thumbs-up"></i>Avis de nos clients</a></li>
+                                    <li><a href="#contact"><i class="fa fa-phone-square"></i> Contact</a></li>
+                                </ul>
+                            </div>  <!-- /.navbar-collapse -->
+                        </nav>
+                    </div>
+                </header>	<!-- site-header -->
+                
+                <div class="middle text-center clearfix">
+                    <div class="container-fuild">
+                        <h1 class="pro-name">Développeur<!-- <span style="color: #71b644;" > --> web <!-- </span> --></h1>
+                        <p class="tagline">Donnez vie à vos rêves</p>
+                       
+                       
+                    </div>  <!-- container -->
+                </div>  <!-- middle -->
+                
+                <div class="bottom text-center">
+                    <a href="#portfolio"><i class="fa fa-angle-down fa-3x pulse"></i></a>
+                </div>
+            </div>  <!-- overlay-1 -->
+        </div>  <!-- content-block -->
+
+        <div class="content-block text-center" id="portfolio">
+            <header class="block-heading cleafix">
+                <h1>Mes Derniers travaux</h1>
+                   <bouton> <a class="btn btn-sm btn-primary" href="img/CV_ElmiDiraneh.pdf">Télecharge ici mon CV</a></bouton>
+                            
+            </header>
+
+                   <section class="content-section" id="portfolio">
+    <div class="container">
+      <div class="content-section-heading text-center">
+       
+      </div>
+         
+      <div class="row no-gutters">
+      <div class="col-lg-6">
+          <a class="portfolio-item" href="https://roblehe.promo-44.codeur.online/projet01boostrape-main/projetboostrape.html" >
+              <div class="caption">
+              <div class="caption-content">
+                <div class="h2">We Design and Develop</div>
+               
+              </div>
+            </div>
+            
+             <img class="img-fluid" src="img/Capture32.JPG"  alt="">
+          </a>
+        </div>
+        <div class="col-lg-6">
+          <a class="portfolio-item" href="https://roblehe.promo-44.codeur.online/web/index.html">
+            <div class="caption">
+              <div class="caption-content">
+                <div class="h2">Burger Cafe Restaurant</div>
+               
+              </div>
+            </div>
+            <img class="img-fluid" src="img/Capture.JPG" alt="">
+          </a>
+        </div>
+       
+        <div class="col-lg-6">
+          <a class="portfolio-item" href="https://roblehe.promo-44.codeur.online/projet01ligne/PageAcceuil.php">
+            <div class="caption">
+              <div class="caption-content" >
+                <div class="h2">Hertz Location</div>
+               
+              </div>
+            </div>
+            <img class="img-fluid" src="img/Capturedb.JPG" alt="">
+          </a>
+        </div>
+        <div class="col-lg-6" >
+          <a class="portfolio-item" href="https://roblehe.promo-44.codeur.online/CREATIVES-main/index.html">
+            <div class="caption">
+              <div class="caption-content">
+                <div class="h2">CREATIVES</div>
+                
+              </div>
+            </div>
+            <img class="img-fluid" src="img/projet03.JPG"  alt="">
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+       <!--  <div class="content-block text-center" id="services">
+            <div class="overlay-2">
+                        <header class="block-heading cleafix">
+                            <h1>More About Me</h1>
+                            <p>Lorem Ipsum Text</p>
+                        </header>
+                        <div class="block-content">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                <div class="row ">
+                                    <div class="col-sm-4">
+                                        <h4 class="pro-stat">Completed Project</h4>
+                                        <h2 class="proj-name count1 count-timer" >020</h2>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <h4 class="pro-stat">Working Project</h4>
+                                        <h2 class="proj-name count2">9</h2>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <h4 class="pro-stat">Top Rated Project</h4>
+                                        <h2 class="proj-name count3">015</h2>
+                                    </div>
+                                </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>    overlay-2
+        </div>  --> <!-- content-block -->
+        <div id="services" class="content-block">
+            <div id="numbers" class="parallax">
+                <div class="overlay">
+                    <!-- title -->
+                    <div class="container-fluid numbers-title">
+                        <div class="container">
+                            <div class="row block-heading">
+                                <h1>Quelques faits intéressants sur moi</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container-fluid">
+                        <!-- number list -->
+                        <ul class="numbersList">
+                            <li class="col-md-2 col-sm-4 col-xs-6">
+                                <h4> Nombre de Site Web Développer </h4>
+                                <span id="number1" class="count1 count-timer">101</span>
+                            </li>
+                            <li class="col-md-2 col-sm-4 col-xs-6">
+                                <h4> Ligne de code </h4>
+                                <span id="number2" class="count2">3000</span>
+                            </li>
+                            <li class="col-md-2 col-sm-4 col-xs-6">
+                                <h4> Nombre d'heur de travail</h4>
+                                <span id="number3"  class="count3">54</span>
+                            </li>
+                            <li class="col-md-2 col-sm-4 col-xs-6">
+                                <h4> Dépôts dans le Git</h4>
+                                <span id="number4" class="count4">99</span>
+                            </li>
+                            <li class="col-md-2 col-sm-4 col-xs-6">
+                                <h4>Application Mobile</h4>
+                                <span id="number5" class="count5">124</span>
+                            </li>
+                            <li class="col-md-2 col-sm-4 col-xs-6">
+                                <h4>Clients Stisfait</h4>
+                                <span id="number6" class="count6">2000</span>
+                            </li>
+                        </ul><!-- numbersList end -->
+                    </div><!-- container-fluid end -->
+                </div><!-- overlay end -->
+            </div>
+        </div>
+            
+
+        <div class="content-block" id="testimonials">
+            <header class="block-heading cleafix text-center">
+                <h1>Avis de nos clients</h1>
+                <!-- <p>Lorem Ipsum Text</p> -->
+            </header>
+            <div class="block-content text-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">
+                        	<div class="owl-carousel">
+								<div class="owl-item">
+									<div class="testimonial">
+                                        <img alt="Client Photo" src="img/testimonial_31-190x190.jpg">
+                                        <p>In at accumsan risus. Nam id volutpat ante. Etiam vel mi mattis, vulputate nunc nec, sodales nibh. Etiam nulla magna, gravida eget ultricies sit amet.</p>
+                                        <strong>Jhon Doe</strong><br>
+                                        <span>Head of Ideas, Technext</span>
+                                    </div>
+								</div>	<!-- owl-item -->
+								<div class="owl-item">
+									<div class="testimonial">
+                                        <img alt="Client Photo" src="img/testimonial_11-190x190.jpg">
+                                        <p>In at accumsan risus. Nam id volutpat ante. Etiam vel mi mattis, vulputate nunc nec, sodales nibh. Etiam nulla magna, gravida eget ultricies sit amet.</p>
+                                        <strong>Jane Doe</strong><br>
+                                        <span>CEO, Apple Inc</span>
+                                    </div>
+								</div>	<!-- owl-item -->
+								<div class="owl-item">
+									<div class="testimonial">
+                                        <img alt="Client Photo" src="img/testimonial_22-190x190.jpg">
+                                        <p>In at accumsan risus. Nam id volutpat ante. Etiam vel mi mattis, vulputate nunc nec, sodales nibh. Etiam nulla magna, gravida eget ultricies sit amet.</p>
+                                        <strong>Albert Doe</strong><br>
+                                        <span>Team Lead, Design Studio</span>
+                                    </div>
+								</div>	<!-- owl-item -->
+							</div>	<!-- owl-carousel -->
+						</div>	<!-- col-md-12 -->
+                    </div>	<!-- row -->
+                </div>	<!-- container -->
+            </div>	<!-- block-content -->
+        </div>	<!-- content-block -->
+
+        <div class="content-block" id="contact">
+            <div class="overlay-3">
+                <header class="block-heading cleafix text-center">
+                    <h1>Contact</h1>
+                   
+                </header>
+                <div class="block-content text-center">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-6 wow animated fadeInLeft">
+                                <form class="contact-form" method="POST" >
+                                    <input type="text" name="name" placeholder="Nom" required>
+                                    <input type="email" name="email" placeholder="Email" required>
+                                    <textarea rows="5" name="message" placeholder="Message" required></textarea>
+                                    <input type="submit" value="Submit">
+                                </form>
+                                <?php
+if(isset($_POST['message'])){
+$entete = 'MIME-Version: 1.0' . "\r\n";
+$entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+$entete .= 'From: ' . $_POST['email'] . "\r\n";
+
+$message = '<h1>Message envoyé depuis la page Contact</h1>
+<p><b>Nom : </b>' . $_POST['name'] . '<br>
+<b>Email : </b>' . $_POST['email'] . '<br>
+<b>Message : </b>' . $_POST['message'] . '</p>';
+
+$retour = mail('diraneh1@hotmail.com', 'Envoi depuis page Contact', $message, $entete);
+if($retour) {
+echo '<p>Votre message a bien été envoyé.</p>';
+}
+}
+?>
+
+                            </div>
+                            <div class="col-md-6 wow animated fadeInRight">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="contact-info">
+                                            <div class="clearfix">
+                                                <div class="rotated-icon">
+                                                    <div class="sqaure-nebir"></div>
+                                                    <i class="fa fa-map-marker"></i>
+                                                </div>
+                                                <p><strong> Addresse:</strong> 14 VOIE CITE VIOTTE 25000 Besançon.
+                                                </p>
+                                            </div>
+                                            <div class="clearfix">
+                                                <div class="rotated-icon">
+                                                    <div class="sqaure-nebir"></div>
+                                                    <i class="fa fa-mobile"></i>
+                                                </div>
+                                                <p><strong> Numéro :</strong> +33751413840 </p>
+                                            </div>
+                                            <div class="clearfix">
+                                                <div class="rotated-icon">
+                                                    <div class="sqaure-nebir"></div>
+                                                    <i class="fa fa-envelope-o"></i>
+                                                </div>
+                                                <p>
+                                                    <strong> Email:</strong> r.elmidiraneh@codeur.online
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <ul class="social-box">
+                                       
+                                        <li><a class="twitter-icon" href="#"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                                        <li><a class="g-plus-icon" href="#"><i class="fa fa-google-plus"></i></a></li>
+                                        <li><a class="linkedin-icon" href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>	<!-- block-content -->
+            </div>	<!-- overlay-3 -->
+        </div>	<!-- content-block -->
+
+        <footer id="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="copyright">&copy; 2020</div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="Conçu par ">Développé par Elmi Diraneh </div>
+                    </div>
+                </div>
+            </div>
+        </footer>	<!-- site-footer -->
+
+
+        <!-- js -->
+        <script>
+            new WOW().init();
+        </script>
+
+        <script src="js/jquery-2.1.3.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.actual.min.js"></script>
+        <script src="js/isotope.pkgd.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery.isonscreen.js"></script>
+       
+        <script src="js/main.js"></script>
+
+        <script>
+        	$(document).ready(function(){
+  				$('.owl-carousel').owlCarousel({
+    				loop:true,
+    				margin:10,
+    				autoplay:true,
+    				autoplayTimeout:3000,
+    				autoplayHoverPause:true,
+    				responsiveClass:true,
+    				responsive:{
+        					0:{
+					            items:1,
+        					},
+					        600:{
+					            items:1,
+					        },
+					        1000:{
+					            items:1,
+					        }
+    				}
+				})
+			});
+        </script>
+
+	</body>
+</html>
